8f93cffb5c8e8a20d28972bfdcde9293095f6320
siya-pandey-bluelupin <siya@bluelupin.com>
2024-09-26 18:19:31 +0530

    two way binding for player names

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Player from "./components/Player";
+
+function App() {
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player initialName="player 1" symbol='X'/>
+          <Player initialName="player 2" symbol='O'/>
+         
+
+        </ol>
+        GAME BOARD
+      </div>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/Player.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+const Player = ({ initialName, symbol }) => {
+  const [isEditing, setIsEditing] = useState(false);
+const [playerName,setPlayerName]=useState(initialName);
+  const handleEditClick = () => {
+    setIsEditing( editing=>!editing);
+  };
+  const handleChange=(event)=>{
+    setPlayerName(event.target.value);
+ }
+  let editablePlayerName=<span className="player-name">{playerName}</span>
+ if (isEditing){
+    editablePlayerName=<input type="text" required value={playerName} onChange={handleChange} />
+ }
+
+
+  return (
+    <li>
+      <span className="player">
+{editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing?'Save':'Edit'}</button>
+    </li>
+  );
+};
+
+export default Player;
